c41434d1a5e6828a6957d434fef7eba455189626
Matthew Virga <mgvirga@gmail.com>
2019-10-19 21:26:40 -0400

    -Changes -Made a simple system to manage panels and buttons in the canvas -Started a simple script for the ball reaching the goal for how that will correlate to points only -Started some of the button functionality -Modified a simple rim shader from unity documentation -Made a canvas for all panels we will use -Made timer functionalities -Started an alternate ar placement script for the throwing trash level

---
+++ Unity AR Test/Assets/Scripts/BallScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    SystemManager _systemManager;
+
+    void Start()
+    {
+        _systemManager = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<SystemManager>();
+    }
+
+    IEnumerator SetInactive()
+    {
+        yield return new WaitForSeconds(10);
+        gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "goal")
+        {
+            _systemManager.Score = _systemManager.Score += 10;
+        }
+    }
+
+    void Update()
+    {
+        if (gameObject.activeInHierarchy)
+            StartCoroutine(SetInactive());
+        else
+            StopCoroutine(SetInactive());
+    }
+}
---
+++ Unity AR Test/Assets/Shaders/Rim.shader
@@ -0,0 +1,23 @@
+﻿Shader "Matt/Rim" {
+	Properties{
+	  _RimColor("Rim Color", Color) = (0.26,0.19,0.16,0.0)
+	  _RimPower("Rim Power", Range(0.5,8.0)) = 3.0
+	}
+		SubShader{
+		  Tags { "RenderType" = "Opaque" }
+		  CGPROGRAM
+		  #pragma surface surf Lambert
+		  struct Input {
+			  float3 viewDir;
+		  };
+
+		  float4 _RimColor;
+		  float _RimPower;
+		  void surf(Input IN, inout SurfaceOutput o) {
+			  half rim = 1.0 - saturate(dot(normalize(IN.viewDir), o.Normal));
+			  o.Emission = _RimColor.rgb * pow(rim, _RimPower);
+		  }
+		  ENDCG
+	  }
+		  Fallback "Diffuse"
+}
