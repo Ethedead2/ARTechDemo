e22766f0be90a5dbfea56d740550a30df1e7ea92
Matthew Virga <mgvirga@gmail.com>
2019-11-02 15:01:09 -0400

    test

--- Unity AR Test/Assets/Scripts/BallScript.cs
+++ Unity AR Test/Assets/Scripts/BallScript.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class BallScript : MonoBehaviour
 {
 
     SystemManager _systemManager;
-
+    
     GameObject bucket;
-
+    public Vector2 startPos;
+    //public Vector2 direction;
+    public bool directionChosen;
     private bool thrown = false; //if ball has been thrown, prevents 2 or more balls
     private Vector3 force;
     private Rigidbody ballRB;
+    bool touch;
 
     void Start()
     {
@@ -50,34 +53,43 @@ public class BallScript : MonoBehaviour
 
     void Update()
     {
-        if (gameObject.activeInHierarchy)
+        // Track a single touch as a direction control.
+        if (Input.touchCount > 0)
         {
-            if (!thrown && Input.GetMouseButtonDown(0))
-            {
+            Touch touch = Input.GetTouch(0);
 
-                thrown = true;
-                ballRB.constraints = RigidbodyConstraints.None;
-                //Sets the mouse pointers vector3
-                transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.y);
-                mousePreviousLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.y);
-            }
-            else if (Input.GetMouseButton(0))
+            // Handle finger movements based on touch phase.
+            switch (touch.phase)
             {
-                mouseCurLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.y);
-                force = mouseCurLocation - mousePreviousLocation;//Changes the force to be applied
-                mousePreviousLocation = mouseCurLocation;
-                force = force.normalized * 10;
-                ballRB.AddForce(force, ForceMode.Impulse);
-            }
-            else if (Input.GetMouseButtonUp(0))
-            {
-                thrown = false;
-                StartCoroutine(SetInactive());
+                // Record initial touch position.
+                case TouchPhase.Began:
+                    startPos = touch.position;
+                    directionChosen = false;
+                    thrown = true;
+                    ballRB.constraints = RigidbodyConstraints.None;
+                    //Sets the mouse pointers vector3
+                    transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.y);
+                    mousePreviousLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.y);
+                    break;
 
+                    // Determine direction by comparing the current touch position with the initial one.
+                    case TouchPhase.Moved:
+                    //direction = touch.position - startPos;
+                    mouseCurLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.y);
+                    force = mouseCurLocation - mousePreviousLocation;//Changes the force to be applied
+                    mousePreviousLocation = mouseCurLocation;
+                    force = force.normalized * 10;
+                    ballRB.AddForce(force, ForceMode.Impulse);
+                        break;
+
+                // Report that a direction has been chosen when the finger is lifted.
+                case TouchPhase.Ended:
+                    directionChosen = true;
+                    thrown = false;
+                    StartCoroutine(SetInactive());
+                    break;
             }
         }
-        else
-            StopCoroutine(SetInactive());
     }
 
     public bool Thrown
